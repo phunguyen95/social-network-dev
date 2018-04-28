5202b79d3953676d525d9f2feb193798cad06f67
Phu Nguyen <phu.nguyen09995@gmail.com>
2018-04-28 18:29:06 +1200

    implement logically delete

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,40 @@
 const express = require('express');
 const router = express.Router();
-
+//load user model
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 router.get('/test', (req, res) => {
   res.json({ message: 'users works' });
+});
+
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already exist' });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', //size
+        r: 'pg', //ratimg
+        d: 'mm' //default
+      });
+      const newUser = new User({
+        email: req.body.email,
+        name: req.body.name,
+        avatar,
+        password: req.body.password
+      });
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json({ user }))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -3,8 +3,16 @@ const mongoose = require('mongoose');
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
+const bodyParser = require('body-parser');
 
 const app = express();
+//body parser middlewaer
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
 //db config
 const db = require('./config/keys').mongoURI;
 //connect to mongoosedb
